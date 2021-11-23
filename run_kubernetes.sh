191309bf25e95c70313df6085d3477a3148aa839
Nikhil Sarecha <nikhilsarecha@gmail.com>
2021-11-23 19:10:06 +0000

    first completion

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -6,13 +6,20 @@
 # This is your Docker ID/path
 # dockerpath=<>
 
+dockerpath=nikhilsarecha/proj4
+
+
 # Step 2
 # Run the Docker Hub container with kubernetes
 
+kubectl create deployment udacity-project4 --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
 
+kubectl port-forward udacity-project4-f6bb7b548-j5j6g 8000:80
